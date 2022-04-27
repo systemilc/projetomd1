59c5dcb7d1a17926b070adb06466d035aa80d411
systemilc <isaac.systemilc@gmail.com>
2022-04-27 11:17:31 -0300

    projeto final MD1

---
+++ anubis/js/fase3.js
@@ -0,0 +1,21 @@
+let vida = 3
+
+function pertunta() {
+    var resposta = prompt("Digite a sua resposta!");
+    for (var vida = 0; vida < 2; vida++) {
+        if (resposta == "peixe" || resposta == "PEIXE") {
+            window.location.href = "../vitoria.html";
+        } else {
+            vida - 1
+            alert(`Errado! Não se preocupe, tente novamente. Este é o seu ${vida + 1}° erro`);
+
+            resposta = prompt("Digite a sua resposta!");
+        }
+    }
+    if (resposta == "peixe" || resposta == "PEIXE") {
+        window.location.href = "../vitoria.html";
+    } else {
+
+        window.location.href = "../game_over.html";
+    }
+}
